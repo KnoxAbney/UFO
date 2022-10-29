e4c6809398e1373c6ae7a0f054cbad0f88877359
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2022-10-29 18:08:43 -0400

    Update UfoFunc.hpp

--- UfoFunc.hpp
+++ UfoFunc.hpp
@@ -4,8 +4,9 @@
 void displaymisses(int misses);
 std::string setup();
 std::vector <char> StringToVector(std::string passcode);
-void testoutput(std::vector <char> phrase);
+void printoutput(std::vector <char> phrase);
 char P2inp();
+std::vector <char> GuessesUpdate(char guess, std::vector <char> guesses);
 bool guesscorrect(char guess, std::string passcode, std::vector <char> output);
 std::vector <char> update(char guess, std::string passcode, std::vector <char> output);
 bool endtest(int misses, std::string passcode, std::vector <char> output);
