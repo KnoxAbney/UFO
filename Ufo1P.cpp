bbfda153cdfa96009b03dcfc9c302b5289b8c16c
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2022-10-29 18:06:57 -0400

    Update Ufo1P.cpp

--- Ufo1P.cpp
+++ Ufo1P.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
-#include "UfoFunc.hpp"
+#include "tttFunc.hpp"
 
 int main() {
-  // initialize necessary vairables
+  // initialize necessary vairables~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   std::string passcode = SPSetup();
   int misses = 0;
   bool end = false;
   std::vector <char> output = StringToVector(passcode);
+  std::vector <char> guesses = {' '};
   
 
   // Performs main task using functions defined in "UfoFunc.cpp" and declared in UfoFunc.hpp ~~~~~~~~~~~~~~~~~
   while(end != true){
-    testoutput(output);
+    int i = 0;
+    printoutput(output);
     char guess = P2inp();
+    guesses = GuessesUpdate(guess, guesses);
     bool correct = guesscorrect(guess, passcode, output);
     system("clear");
+    std::cout << "Guesses so far:";
+    printoutput(guesses);
     if(correct == true){
       output = update(guess, passcode, output);
     } 
@@ -23,5 +28,6 @@ int main() {
       displaymisses(misses);
     }
     end = endtest(misses, passcode, output);
+    i++;
   }
 }
