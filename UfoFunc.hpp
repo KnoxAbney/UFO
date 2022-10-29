b418efffc76b7e32064a4c54c896043455958bdb
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2022-10-28 23:20:30 -0400

    Create UfoFunc.hpp
    declare functions to be used in Ufo.cpp

---
+++ UfoFunc.hpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+// So many declarations
+void displaymisses(int misses);
+std::string setup();
+std::vector <char> P1input(std::string passcode);
+void testoutput(std::vector <char> phrase);
+char P2inp();
+bool guesscorrect(char guess, std::string passcode, std::vector <char> output);
+std::vector <char> update(char guess, std::string passcode, std::vector <char> output);
+bool endtest(int misses, std::string passcode, std::vector <char> output);
