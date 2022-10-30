788b02fa262cd54bc545ccf570970d36dd2acc82
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2022-10-29 20:28:10 -0400

    Update UfoFunc.hpp
    added new function declaration. couldn't get template to work properly. Might fix later.

--- UfoFunc.hpp
+++ UfoFunc.hpp
@@ -4,10 +4,11 @@
 void displaymisses(int misses);
 std::string setup();
 std::vector <char> StringToVector(std::string passcode);
-void printoutput(std::vector <char> phrase);
-char P2inp();
-std::vector <char> GuessesUpdate(char guess, std::vector <char> guesses);
-bool guesscorrect(char guess, std::string passcode, std::vector <char> output);
-std::vector <char> update(char guess, std::string passcode, std::vector <char> output);
+void printoutput1(std::vector <char> output);
+void printoutput2(std::vector <std::string> output);
+std::string P2inp();
+std::vector <std::string> GuessesUpdate(std::string guess, std::vector <std::string> guesses);
+bool guesscorrect(std::string guess, std::string passcode, std::vector <char> output);
+std::vector <char> update(std::string guess, std::string passcode, std::vector <char> output);
 bool endtest(int misses, std::string passcode, std::vector <char> output);
 std::string SPSetup();
