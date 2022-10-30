24d5c7488f6889be400acc3c2da05de387ae23d3
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2022-10-29 20:25:34 -0400

    Update Ufo2P.cpp
    adds option to guess full phrase and comments explaining each section

--- Ufo2P.cpp
+++ Ufo2P.cpp
@@ -11,13 +11,27 @@ int main() {
 
   // Performs main task using functions defined in "UfoFunc.cpp" and declared in UfoFunc.hpp ~~~~~~~~~~~~~~~~~
   while(end != true){
-    printoutput(output);
-    char guess = P2inp();
+    printoutput1(output);
+
+    // Asks for next guess, adds to guess list, and checks for correctness ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    std::string guess = P2inp();
     guesses = GuessesUpdate(guess, guesses);
     bool correct = guesscorrect(guess, passcode, output);
+
+    // Allows for guesses of entire phrase ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    if((guess.size() != 1) && (correct == true)){
+      clear;
+      std::cout << passcode << "\n";
+      std::cout << "The humans win!";
+      break;
+    }
+
+    //clears and resets previous guess printout for next guess ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     system("clear");
     std::cout << "Guesses so far:";
-    printoutput(guesses);
+    printoutput2(guesses);
+
+    // checks for correctness and updates output or misses counter accordingly ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     if(correct == true){
       output = update(guess, passcode, output);
     } 
@@ -25,6 +39,8 @@ int main() {
       misses++;
       displaymisses(misses);
     }
+    
+    // checks for a win condition and ends the program if one is met ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     end = endtest(misses, passcode, output);
   }
 }
